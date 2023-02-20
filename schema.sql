f5ce041ef2ca3923fe3be26395d9e9a188d7d1fe
RoyJumah <royjumah17@gmail.com>
2023-02-20 15:43:41 +0300

    Add tables data

--- schema.sql
+++ schema.sql
@@ -1,5 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals (
-    name varchar(100)
+CREATE TABLE animals(
+id INT,
+name VARCHAR(250),
+date_of_birth DATE,
+escape_attempts INT,
+neutered BOOLEAN,
+weight_kg FLOAT
 );
