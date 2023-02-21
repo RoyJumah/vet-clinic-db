bb63d0b7e0e1e9acd41a004467b0e47e556378db
RoyJumah <royjumah17@gmail.com>
2023-02-21 15:53:36 +0300

    add queries and implement new tasks to the table

--- queries.sql
+++ queries.sql
@@ -7,4 +7,12 @@ SELECT name, escape_attempts FROM ANIMALS WHERE weight_kg > 10.5
 SELECT * FROM ANIMALS WHERE neutered = True;
 SELECT * FROM ANIMALS WHERE name != 'Gabumon';
 SELECT * FROM ANIMALS WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
-
+delete from animals where date_of_birth > '2022-01-01';
+update animals set weight_kg = weight_kg * -1;
+update animals set weight_kg = weight_kg * -1 where weight_kg < 0;
+select count(name) from animals;
+select count(name) from animals where escape_attempts < 1;
+select avg(weight_kg) from animals;
+SELECT neutered, AVG(escape_attempts) AS avg_escape_attempts FROM animals GROUP BY neutered ORDER BY AVG(escape_attempts) DESC LIMIT 1;
+SELECT species, MIN(weight_kg) AS min_weight, MAX(weight_kg) AS max_weight FROM animals GROUP BY species;
+SELECT species, AVG(escape_attempts) AS avg_escape_attempts FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31' GROUP BY species;
